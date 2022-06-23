799c459ffe509a4b73fa0c2463457d66abd6209b
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-06-23 15:33:53 +0300

    AddNatNumRecursion

---
+++ JavaBH/Lesson5/recursiveFunctions/task1/NaturalNumber.java
@@ -0,0 +1,20 @@
+package task1;
+
+public class NaturalNumber {
+
+	public static void main(String[] args) {
+		int n = 25;
+		
+	 nateralNumber(n);
+		
+	}
+	
+	static int nateralNumber(int nN){
+		System.out.println(nN);
+		if (nN == 1) {
+			return 1;
+		}
+		return nateralNumber(nN-1);
+	}
+
+}

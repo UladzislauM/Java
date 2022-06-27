0c8fcbce05b46b7ba6c9fc94449e29bcd4022310
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-06-27 12:00:17 +0300

    AddTask1Lesson6

--- JavaBH/Lesson6/src/Tasks/Deal.java
+++ JavaBH/Lesson6/src/Tasks/Deal.java
@@ -1,7 +1,5 @@
 package Tasks;
 
-import java.util.Collections;
-
 public class Deal {
 
 	public static void main(String[] args) {
@@ -12,8 +10,6 @@ public class Deal {
 		 
 		String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
-		//перетусовка с помощью алгоритмов сортировки
-		
 		String [][] numbersCards = new String[4][14];
 		
 		//Заполняю 2-мерный массив всеми вариантами карт
@@ -61,6 +57,8 @@ public class Deal {
 			}
 		}
 		
+		
+		//перетусовка с помощью алгоритмов сортировки
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 14; j++) {
 				
@@ -71,9 +69,7 @@ public class Deal {
 
 	        }
 		}
-		
 
-	
 		for (int i = 0; i < nunberPiples; i++) {
 			for (int j = 0; j < 5; j++) {
 				numberCardsPiples[i][j] = numbersCards[i][j];
@@ -82,50 +78,5 @@ public class Deal {
 			}
 			System.out.println("\n");
 		}
-		
-		
-
 	}
-	static void heapify(int[] array, int length, int i) {  
-	    int leftChild = 2*i+1;
-	    int rightChild = 2*i+2;
-	    int largest = i;
-
-	    // если левый дочерний больше родительского
-	    if (leftChild < length && array[leftChild] > array[largest]) {
-	        largest = leftChild;
-	    }
-
-	    // если правый дочерний больше родительского
-	    if (rightChild < length && array[rightChild] > array[largest]) {
-	        largest = rightChild;
-	    }
-
-	    // если должна произойти замена
-	    if (largest != i) {
-	        int temp = array[i];
-	        array[i] = array[largest];
-	        array[largest] = temp;
-	        heapify(array, length, largest);
-	    }
-	}
-
-	public static void heapSort(int[] array) {  
-	    if (array.length == 0) return;
-
-	    // Строим кучу
-	    int length = array.length;
-	    // проходим от первого без ответвлений к корню
-	    for (int i = length / 2-1; i >= 0; i--)
-	        heapify(array, length, i);
-
-	    for (int i = length-1; i >= 0; i--) {
-	        int temp = array[0];
-	        array[0] = array[i];
-	        array[i] = temp;
-
-	        heapify(array, i, 0);
-	    }
-	}
-
 }

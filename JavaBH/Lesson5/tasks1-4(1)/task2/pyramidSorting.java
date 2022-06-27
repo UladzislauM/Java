0da42c11504efec56e64aac6c8d0c85a5d0a46a0
Uladzislau Marshalau <102827521+UladzislauM@users.noreply.github.com>
2022-06-27 11:58:25 +0300

    AddTask1Lesson6

--- JavaBH/Lesson5/tasks1-4(1)/task2/pyramidSorting.java
+++ JavaBH/Lesson5/tasks1-4(1)/task2/pyramidSorting.java
@@ -2,19 +2,45 @@ package task2;
 
 public class pyramidSorting {
 
-	public static void bubbleSort(int[] array) {  
-	    boolean sorted = false;
-	    int temp;
-	    while(!sorted) {
-	        sorted = true;
-	        for (int i = 0; i < array.length - 1; i++) {
-	            if (array[i] > array[i+1]) {
-	                temp = array[i];
-	                array[i] = array[i+1];
-	                array[i+1] = temp;
-	                sorted = false;
-	            }
-	        }
+	static void heapify(int[] array, int length, int i) {  
+	    int leftChild = 2*i+1;
+	    int rightChild = 2*i+2;
+	    int largest = i;
+
+	    // если левый дочерний больше родительского
+	    if (leftChild < length && array[leftChild] > array[largest]) {
+	        largest = leftChild;
+	    }
+
+	    // если правый дочерний больше родительского
+	    if (rightChild < length && array[rightChild] > array[largest]) {
+	        largest = rightChild;
+	    }
+
+	    // если должна произойти замена
+	    if (largest != i) {
+	        int temp = array[i];
+	        array[i] = array[largest];
+	        array[largest] = temp;
+	        heapify(array, length, largest);
+	    }
+	}
+
+	public static void heapSort(int[] array) {  
+	    if (array.length == 0) return;
+
+	    // Строим кучу
+	    int length = array.length;
+	    // проходим от первого без ответвлений к корню
+	    for (int i = length / 2-1; i >= 0; i--)
+	        heapify(array, length, i);
+
+	    for (int i = length-1; i >= 0; i--) {
+	        int temp = array[0];
+	        array[0] = array[i];
+	        array[i] = temp;
+
+	        heapify(array, i, 0);
 	    }
 	}
 
--- JavaBH/Lesson6/src/Tasks/Deal.java
+++ JavaBH/Lesson6/src/Tasks/Deal.java
@@ -1,24 +1,131 @@
 package Tasks;
 
+import java.util.Collections;
+
 public class Deal {
 
 	public static void main(String[] args) {
-		int n = 3;
+		int nunberPiples = 3;
+		String numberCardsPiples[][] = new String [nunberPiples][5];
 		
-		String[] suits = {"Пик", "Бубен", "Черв", "Треф"};
+		String[] suits = {"Peak", "Tambourine", "Hearts", "Clubs"};
 		 
-		String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10","Валет", "Королева", "Король", "Туз"};
+		String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
 		//перетусовка с помощью алгоритмов сортировки
+		
+		String [][] numbersCards = new String[4][14];
+		
+		//Заполняю 2-мерный массив всеми вариантами карт
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 14; j++) {
+				switch (i) {
+				case 0: {
+					if(j == 0) {
+						numbersCards[i][j] = suits[i];
+					}
+					else {
+					numbersCards[i][j] = rank[j-1];
+					}
+					break;
+				}
+                case 1: {
+                	if(j == 0) {
+						numbersCards[i][j] = suits[i];
+					}
+					else {
+					numbersCards[i][j] = rank[j-1];
+					}
+                	break;
+				}
+                case 2: {
+					if(j == 0) {
+						numbersCards[i][j] = suits[i];
+					}
+					else {
+					numbersCards[i][j] = rank[j-1];
+					}
+					break;
+				}
+                case 3: {
+                	if(j == 0) {
+						numbersCards[i][j] = suits[i];
+					}
+					else {
+					numbersCards[i][j] = rank[j-1];
+					}
+                	break;
+				}
 
-		for (int i = 0; i < n; i++) {
-			int r = i + (int) (Math.random() * (n-i)); // случайная карта в колоде
+				}
+			}
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 14; j++) {
+				
+				int r = i + (int) (Math.random() * (j - i)); // случайная карта в колоде
+	        String temp = numbersCards[i][r];
+	        numbersCards[i][r] = numbersCards[i][j];
+	        numbersCards[i][j] = temp;
+
+	        }
+		}
+		
+
+	
+		for (int i = 0; i < nunberPiples; i++) {
+			for (int j = 0; j < 5; j++) {
+				numberCardsPiples[i][j] = numbersCards[i][j];
+				System.out.println(numberCardsPiples[i][j]);
+
+			}
+			System.out.println("\n");
 		}
 		
 		
-		
 
+	}
+	static void heapify(int[] array, int length, int i) {  
+	    int leftChild = 2*i+1;
+	    int rightChild = 2*i+2;
+	    int largest = i;
 
+	    // если левый дочерний больше родительского
+	    if (leftChild < length && array[leftChild] > array[largest]) {
+	        largest = leftChild;
+	    }
+
+	    // если правый дочерний больше родительского
+	    if (rightChild < length && array[rightChild] > array[largest]) {
+	        largest = rightChild;
+	    }
+
+	    // если должна произойти замена
+	    if (largest != i) {
+	        int temp = array[i];
+	        array[i] = array[largest];
+	        array[largest] = temp;
+	        heapify(array, length, largest);
+	    }
+	}
+
+	public static void heapSort(int[] array) {  
+	    if (array.length == 0) return;
+
+	    // Строим кучу
+	    int length = array.length;
+	    // проходим от первого без ответвлений к корню
+	    for (int i = length / 2-1; i >= 0; i--)
+	        heapify(array, length, i);
+
+	    for (int i = length-1; i >= 0; i--) {
+	        int temp = array[0];
+	        array[0] = array[i];
+	        array[i] = temp;
+
+	        heapify(array, i, 0);
+	    }
 	}
 
 }
